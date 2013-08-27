4986c5c65b0a67bba40ed7a4b09b30a6cc57545c
Colin Hutchinson <colin@hutchic.com>
2013-08-27 14:56:20 -0400

    Redirect to login page when not authorized

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -40,12 +40,16 @@ app.factory('httpInterceptor', function($q){
   }
 });
 
-app.factory('userProvider', function($q, $http){
+app.factory('userProvider', function($q, $http, $location){
   var user;
   var getUser = function() {
 	var deferred = $q.defer();
 	if(!user){
-      $http.get('https://canvas.rayku.com/api/v1/accounts/1/logins', {cache:true}).then(function(Obj){
+      $http.get('https://canvas.rayku.com/api/v1/accounts/1/logins', {cache:true}).error(function(Obj){
+        if(Obj.status == "unauthenticated"){
+          window.location = "logreg.html";
+        }
+      }).then(function(Obj){
 	    user = Obj.data[0];
         deferred.resolve(user);
       });
